96fdb12d3d407c8cc07e2116860201136ecae8ff
gowthamanniit <karurgowthaman@gmail.com>
2025-04-22 16:30:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,70 @@
+import {useState} from 'react'
+import axios from 'axios'
+function App()
+{
+  /*
+  const [rno,setRno]=useState()
+  const [sname,setSname]=useState("")
+  const [mark,setMark]=useState()
+  */
+  const [inputs,setInputs]=useState({})
+  const [list ,setList]=useState(null)
+
+  const dis=(e)=>{
+    
+    const name=e.target.name
+    const value=e.target.value
+    setInputs(values=>({...values,[name]:value}))
+    /*
+    if (e.target.name==="rno")
+      setRno(e.target.value)
+    if (e.target.name==="sname")
+      setSname(e.target.value)
+    if (e.target.name==="mark")
+      setMark(e.target.value)        
+    */
+  }
+  const insertfun=()=>{
+    /*var inputdata={
+      rno:rno,
+      sname:sname,
+      mark:mark
+    }
+    console.log(inputdata)
+    */
+   console.log(inputs)
+   
+    axios.get("http://127.0.0.1:9339",{params:inputs}).then((res)=>{
+      console.log("response only",res)
+      console.log("res.data only:",res.data)
+      setList(res.data)
+    }).catch((err)=>{
+      console.log(err.response.data)
+      setList(err.response.data)
+    })
+   // cleartextbox()
+  }
+  /*
+const cleartextbox=()=>{
+  setRno("")
+  setSname("")
+  setMark("")
+}*/
+  return(
+    <>
+        <input type="text" onChange={(e)=>dis(e)}  name="rno" placeholder="Enter Rno:"></input><br></br>
+        <input type="text" onChange={(e)=>dis(e)} name="sname" placeholder="Enter Sname:"></input><br></br>
+        <input type="text" onChange={(e)=>dis(e)} name="mark" placeholder="Enter Mark:"></input><br></br>
+        <input type="button" onClick={insertfun} value="Insert/Save"></input>
+        <h1>{list !==null && "Successfully Inserted using Mongoose :"+ list._id}</h1>
+        <h1>{list !==null && list.acknowledged===true && "Successfully Inserted"}</h1>
+        <h1>{list !==null && list.acknowledged===true && "ID:"+list.insertedId}</h1>
+        
+    </>
+  )
+}
+export default App
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +90,4 @@ function App() {
 }
 
 export default App;
+*/
